57aa3489fee20a5501b607cac6ed609a8adaa9fc
lmiogit <yl_mio@sina.com>
2016-06-06 12:03:12 +0800

    test

--- routes/desktop.js
+++ routes/desktop.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('desktop', { title: 'Lmio' });
+  res.render('error', { title: 'Lmio' });
 });
 
 module.exports = router;
