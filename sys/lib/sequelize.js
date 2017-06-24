9b8f11a984da7dd5b344164f4499803a3f7ad4e0
wb_yaolin@ocj.com.cn <wb_yaolin@ocj.com.cn>
2017-06-24 16:09:33 +0800

    new start

--- sys/lib/sequelize.js
+++
@@ -1,7 +0,0 @@
-var Sequelize = require('sequelize');
-var config = require('../config/datebase');
-
-var sequelize = new Sequelize(config.database, config.user, config.pwd,config.detail);
-
-module.exports = sequelize;
-
